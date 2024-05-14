17d8f75526c49e363ceef07b7e8244967e92ebb3
zzihasss <ayyygirl12@gmail.com>
2024-05-14 17:02:35 +0900

    지도 첨부 예정

--- script/script.js
+++ script/script.js
@@ -7,6 +7,7 @@ $('.gnb_web > li').hover(
 	}
 );
 
+// 메뉴 호출
 $('.btnCall').click(function () {
 	$('.menuMo').addClass('on');
 });
@@ -23,36 +24,48 @@ menuSlides.forEach((el, index) => {
 	});
 });
 
-let bg = document.querySelectorAll($('.bg_container>li'));
-console.log($('.bg_container>li'));
-
+// 2번째 슬라이드 메뉴
 let active_index = 0;
 let enable_event = true;
-let len = $('.service_ul>li').length;
-
-console.log($('.service_ul>li'));
-$('.service_ul > li').on('click', function (event) {
+$('.service_ul>li').on('click', function (event) {
 	event.preventDefault();
+	// 현재 활성화클래스가 있는 인덱스를 구함
+
+	// 예: 현재 5번이 보이고 2번을 누르는 상태라면
+	// 현재 활성화 클래스가 있는 인덱스는 = 5
+	if (enable_event) {
+		enable_event = false;
+
+		let current_index = $('.bg_container>li').filter('.on').index();
+		let target_index = $(this).index();
+		active_index = target_index;
+		// 만약 활성화인덱스와 타겟인덱스가 같다면? ==> 해당 li로 이동하게끔
+		if (active_index == current_index) {
+			$('#tabMenu>ul>li>a').removeClass('on');
+			$('.bg_conatiner>li').addClass('on');
+		}
+	}
 });
 
-function show_bg(index) {
-	$('.service_ul > li')
-		.filter('.on')
-		.stop()
-		.animate({ left: '-100%' }, 500, function () {
-			$(this).removeClass('on').hide();
-		});
-	//위 코드는 현재활성화된 슬라이드li를 옆으로(넥스트방향으로) 이동하는 코드
+// 지도
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var options = {
+	//지도를 생성할 때 필요한 기본 옵션
+	center: new kakao.maps.LatLng(37.4895097, 126.7835609), //지도의 중심좌표.
+	level: 1, //지도의 레벨(확대, 축소 정도)
+};
 
-	//이다음코드는 이후 슬라이드가 나타나게하는 코드
-	$('.panel>li')
-		.eq(index)
-		.show()
-		.css({ left: '100%' })
-		.animate({ left: '0%' }, 500, function () {
-			$(this).addClass('on');
-			enable_event = true;
-		});
-	$('.navi>li>a').removeClass('on');
-	$('.navi>li').eq(index).children('a').addClass('on');
-}
+var marker = {
+	position: new kakao.maps.LatLng(37.4895097, 126.7835609),
+	text: '사과나무치과병원', // text 옵션을 설정하면 마커 위에 텍스트를 함께 표시할 수 있습니다
+};
+
+var staticMapContainer = document.getElementById('staticMap'), // 이미지 지도를 표시할 div
+	staticMapOption = {
+		center: new kakao.maps.LatLng(37.4895097, 126.7835609), // 이미지 지도의 중심좌표
+		level: 3, // 이미지 지도의 확대 레벨
+		marker: marker, // 이미지 지도에 표시할 마커
+	};
+
+// 이미지 지도를 생성합니다
+var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
