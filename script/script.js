0046bfd8bcf509da8322c2295cd638518502504a
zzihasss <ayyygirl12@gmail.com>
2024-05-17 17:45:35 +0900

    메인페이지 4/5 완료

--- script/script.js
+++ script/script.js
@@ -55,17 +55,101 @@ var options = {
 	level: 1, //지도의 레벨(확대, 축소 정도)
 };
 
-var marker = {
-	position: new kakao.maps.LatLng(37.4895097, 126.7835609),
-	text: '사과나무치과병원', // text 옵션을 설정하면 마커 위에 텍스트를 함께 표시할 수 있습니다
-};
+var map = new kakao.maps.Map(container, options);
 
-var staticMapContainer = document.getElementById('staticMap'), // 이미지 지도를 표시할 div
-	staticMapOption = {
-		center: new kakao.maps.LatLng(37.4895097, 126.7835609), // 이미지 지도의 중심좌표
-		level: 3, // 이미지 지도의 확대 레벨
-		marker: marker, // 이미지 지도에 표시할 마커
-	};
+var imageSrc = './img/지도-마커.png', // 마커이미지의 주소입니다
+	imageSize = new kakao.maps.Size(50, 50), // 마커이미지의 크기입니다
+	imageOption = { offset: new kakao.maps.Point(25, 50) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
-// 이미지 지도를 생성합니다
-var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
+// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+	markerPosition = new kakao.maps.LatLng(37.4895097, 126.7835609); // 마커가 표시될 위치입니다
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+	position: markerPosition,
+	image: markerImage, // 마커이미지 설정
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+// 상담신청
+
+let form = document.querySelector('#footer');
+let btnSubmit = document.querySelector('input[type=submit]');
+
+btnSubmit.addEventListener('click', (e) => {
+	// 증상
+	if (!isTxt1('cc', 2)) {
+		e.preventDefault();
+	}
+	// 이름
+	if (!isTxt2('username', 1)) {
+		e.preventDefault();
+	}
+	// 연락처
+	if (!isTxt3('phonenumber', 7)) {
+		e.preventDefault();
+	}
+});
+function isTxt1(cc, len) {
+	let input = form.querySelector(`[name=${cc}]`);
+	let txt = input.value;
+	if (txt.length >= 2) {
+		let iserrMsgs = input.closest('td').querySelectorAll('p');
+		if (iserrMsgs.length > 0) {
+			input.closest('td').querySelector('p').remove();
+		}
+		return true;
+	} else {
+		let iserrMsgs = input.closest('td').querySelectorAll('p');
+		if (iserrMsgs.length > 0) {
+			input.closest('td').querySelector('p').remove();
+		}
+		let errMsg = document.createElement('p');
+		errMsg.append(`증상을 2글자 이상 입력하세요.`);
+		input.closest('td').append(errMsg);
+		return false;
+	}
+}
+function isTxt2(username, len) {
+	let input = form.querySelector(`[name=${username}]`);
+	let txt = input.value;
+	if (txt.length >= 1) {
+		let iserrMsgs = input.closest('td').querySelectorAll('p');
+		if (iserrMsgs.length > 0) {
+			input.closest('td').querySelector('p').remove();
+		}
+		return true;
+	} else {
+		let iserrMsgs = input.closest('td').querySelectorAll('p');
+		if (iserrMsgs.length > 0) {
+			input.closest('td').querySelector('p').remove();
+		}
+		let errMsg = document.createElement('p');
+		errMsg.append(`이름을 정확히 입력하세요.`);
+		input.closest('td').append(errMsg);
+		return false;
+	}
+}
+function isTxt3(phonenumber, len) {
+	let input = form.querySelector(`[name=${[phonenumber]}]`);
+	let txt = input.value;
+	if (txt.length >= 7) {
+		let iserrMsgs = input.closest('td').querySelectorAll('p');
+		if (iserrMsgs.length > 0) {
+			input.closest('td').querySelector('p').remove();
+		}
+		return true;
+	} else {
+		let iserrMsgs = input.closest('td').querySelectorAll('p');
+		if (iserrMsgs.length > 0) {
+			input.closest('td').querySelector('p').remove();
+		}
+		let errMsg = document.createElement('p');
+		errMsg.append(`연락처를 정확히 입력하세요.`);
+		input.closest('td').append(errMsg);
+		return false;
+	}
+}
